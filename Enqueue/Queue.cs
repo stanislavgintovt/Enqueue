3ccecfc66239dd659895ebe88c3c954f744184db
stanislavgintovt <stanislavgintovt2004@gmail.com>
2025-04-11 14:03:34 +0300

    Добавьте файлы проекта.

---
+++ Enqueue/Queue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enqueue
+{
+    class Queue
+    {
+        class Node
+        {
+            Node next;
+            int priority;
+        }
+    }
+}
